5c6fbb6331d8d7624a666817f1f83d942fc29350
name <email>
2017-03-18 17:49:11 +0000

    earth map PogChamp

--- earthMap.js
+++ earthMap.js
@@ -1,27 +1,27 @@
 function generateEarthMap(height){
 	var map = new Map(Math.round(height * 2.3),height);
 	
-	// var imgcanvas = document.createElement('canvas');
-	// var imgctx = imgcanvas.getContext("2d");
-	// var img = document.getElementById("earthimg");
-	// imgcanvas.width = img.width;
-	// imgcanvas.height = img.height;
-	// img.src="earth-map.gif";
-	// imgctx.drawImage(img,0,0);
-	
-	// var data = imgctx.getImageData(0, 0, img.width, img.height);
-	
-	var img = document.createElement("img");
+	var imgcanvas = document.createElement('canvas');
+	var imgctx = imgcanvas.getContext("2d");
+	var img = document.getElementById("earthimg");
+	imgcanvas.width = img.width;
+	imgcanvas.height = img.height;
 	img.src="earth-map.gif";
-	var data = img.getImageData(0,0,img.width,img.height);
+	imgctx.drawImage(img,0,0);
 	
-	var x = 200;
-	var y = 20;
-	var red = data.data[((img.width * y) + x) * 4];
-	var green = data.data[((img.width * y) + x) * 4 + 1];
-	var blue = data.data[((img.width * y) + x) * 4 + 2];
-	var alpha = data.data[((img.width * y) + x) * 4 + 3];
-	console.log(red + "," + green + "," + blue);
+	var data = imgctx.getImageData(0, 0, img.width, img.height);
+	
+	
+	console.log(getPixelRGB(data,20,200,img.width,img.height));
+	
+	
 	
 	console.log(data);
+}
+function getPixelRGB(data,x,y,width,height){
+	var red = data.data[((width * y) + x) * 4];
+	var green = data.data[((width * y) + x) * 4 + 1];
+	var blue = data.data[((width * y) + x) * 4 + 2];
+	var alpha = data.data[((width * y) + x) * 4 + 3];
+	return {r:red,g:green,b:blue,a:alpha};
 }
