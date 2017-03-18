a84ba5b6a9827467dd69c939a8a887390ed2b207
Marcus Kading <m.kading@outlook.com>
2017-03-18 16:32:21 +0000

    random color select

--- cell.js
+++ cell.js
@@ -1,5 +1,5 @@
 function Cell(r,g,b){
-	this.r = r;
-	this.g = g;
-	this.b = b;
+	var this.colors[5] = ["Crimson", "Gold", "LightSalmon", "MediumSlateBlue", "AliceBlue"];
+	var this.color = colors[Math.random(0, 4)];
+	var this.alive = true;
 }
