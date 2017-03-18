f1aacd56922df922af3b7116684f1f3d89596dc8
alex <kalininator@gmail.com>
2017-03-18 22:05:31 +0000

    yay :)

--- spawner.js
+++ spawner.js
@@ -1,9 +1,13 @@
 function Spawner(map, color, x, y){
 	var xArr = [x-1, x, x-1, x+1, x, x+1];
 	var yArr = [y-1, y-1, y, y, y+1, y+1];
-	for(var i = 0; i < getRandomInt(0,7); i++){
-		var a = getRandomInt(0,7)
-		map.cells[xArr[a] >= 0 ? xArr[a] % map.width : map.width + xArr[a]][yArr[a] >= 0 ? yArr[a] % map.height : map.height + yArr[a]].alive = true;
+	var rand = getRandomInt(0,5);
+	for(var i = 0; i < rand; i++){
+		var a = getRandomInt(0,5)
+		var ax = xArr[a] >= 0 ? xArr[a] % map.width : map.width + xArr[a];
+		var ay = yArr[a] >= 0 ? yArr[a] % map.height : map.height + yArr[a];
+		console.log(ax + "," + ay);
+		map.cells[ax][ay].alive = true;
 		map.cells[xArr[a] >= 0 ? xArr[a] % map.width : map.width + xArr[a]][yArr[a] >= 0 ? yArr[a] % map.height : map.height + yArr[a]].color = this.color;
 	}
 }
