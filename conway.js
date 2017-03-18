8bec48a0f34d3ee6c3661ce683a20606e6c33d94
Marcus Kading <m.kading@outlook.com>
2017-03-18 19:22:20 +0000

    if(updated)

--- conway.js
+++ conway.js
@@ -17,10 +17,10 @@ function Conway(map){
 	}
 	for(var a = 0; x < width; x++){
 		for(var b = 0; y < height; y++){
-			if(nCount[a][b] < 2 || nCount[a][b] > 2){ // don't forget to add land qualifier
-				map.cells[a][b].alive = false;
-			}else {
+			if((nCount[a][b] == 2 || nCount[a][b] == 3) && map.landmap.cells[a][b] == 1){ // don't forget to add land qualifier
 				map.cells[a][b].alive = true;
+			}else {
+				map.cells[a][b].alive = false;
 			}	
 		}
 	}
