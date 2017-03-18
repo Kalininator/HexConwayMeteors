8cd74076c1cb92cd03cde3d928932bacf01d8fc7
Marcus Kading <m.kading@outlook.com>
2017-03-18 21:59:57 +0000

    :o:

--- spawner.js
+++ spawner.js
@@ -1,10 +1,9 @@
 function Spawner(map, color, x, y){
 	var xArr = [x-1, x, x-1, x+1, x, x+1];
 	var yArr = [y-1, y-1, y, y, y+1, y+1];
-	
-	for(var i = 0; i < map.getRandomInt(0,7); i++){
+	for(var i = 0; i < getRandomInt(0,7); i++){
 		var a = getRandomInt(0,7)
-		map.cells[xArr[a] >= 0 ? xArr[a] % map.width : map.width + xArr[a]][yArr[b] >= 0 ? yArr[b] % map.height : map.height + yArr[b]].alive = true;
-		map.cells[xArr[a] >= 0 ? xArr[a] % map.width : map.width + xArr[a]][yArr[b] >= 0 ? yArr[b] % map.height : map.height + yArr[b]].color = this.color;
+		map.cells[xArr[a] >= 0 ? xArr[a] % map.width : map.width + xArr[a]][yArr[a] >= 0 ? yArr[a] % map.height : map.height + yArr[a]].alive = true;
+		map.cells[xArr[a] >= 0 ? xArr[a] % map.width : map.width + xArr[a]][yArr[a] >= 0 ? yArr[a] % map.height : map.height + yArr[a]].color = this.color;
 	}
 }
